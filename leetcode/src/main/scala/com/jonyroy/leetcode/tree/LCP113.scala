a607c19db01006fb45bc3eadd05328272d792de4
jonyroy <jonyroyacc@gmail.com>
2022-02-27 14:28:10 +0600

    enable-github-actions

---
+++ leetcode/src/main/scala/com/jonyroy/leetcode/tree/LCP113.scala
@@ -0,0 +1,17 @@
+package com.jonyroy.leetcode.tree
+
+import com.jonyroy.leetcode.common.TreeNode
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ * var value: Int = _value
+ * var left: TreeNode = _left
+ * var right: TreeNode = _right
+ * }
+ *
+ * https://leetcode.com/problems/path-sum-ii/
+ */
+object LCP113 {
+  def pathSum(root: TreeNode, targetSum: Int): List[List[Int]] = ???
+}
---
+++ leetcode/src/test/scala/com/jonyroy/leetcode/tree/LCP113Test.scala
@@ -0,0 +1,11 @@
+package com.jonyroy.leetcode.tree
+
+import com.typesafe.scalalogging.StrictLogging
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class LCP113Test extends AnyFlatSpec with Matchers with StrictLogging {
+
+  it should "return  Path Sum of targeted value" in {}
+
+}
