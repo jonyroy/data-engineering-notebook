d61dd94233f8704558b3af5ccc757191f9934fd8
jonyroy <jonyroyacc@gmail.com>
2022-02-27 15:06:03 +0600

    add-problem-113

--- leetcode/src/main/scala/com/jonyroy/leetcode/tree/LCP113.scala
+++ leetcode/src/main/scala/com/jonyroy/leetcode/tree/LCP113.scala
@@ -13,5 +13,23 @@ import com.jonyroy.leetcode.common.TreeNode
  * https://leetcode.com/problems/path-sum-ii/
  */
 object LCP113 {
-  def pathSum(root: TreeNode, targetSum: Int): List[List[Int]] = ???
+
+  def pathSum(root: TreeNode, targetSum: Int): List[List[Int]] = {
+
+    def findPathSum(root: TreeNode, targetSum: Int, currentSum: Int, paths: List[Int]): List[List[Int]] = {
+
+      if (root != null && root.left == null && root.right == null) {
+        if (currentSum + root.value == targetSum) return List((root.value :: paths).reverse) else return List.empty
+      }
+
+      if (root == null) return List.empty
+
+      val left = findPathSum(root.left, targetSum, currentSum + root.value, root.value :: paths)
+      val right = findPathSum(root.right, targetSum, currentSum + root.value, root.value :: paths)
+
+      left ++ right
+    }
+
+    findPathSum(root, targetSum, 0, List.empty)
+  }
 }
